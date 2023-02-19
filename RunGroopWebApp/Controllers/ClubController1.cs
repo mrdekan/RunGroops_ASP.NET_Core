8d5cbeff1342ec4dd878ef271a7d934c62defbba
mrdekan <yeros05040504@gmail.com>
2023-02-19 19:35:17 +0200

    Added controllers

---
+++ RunGroopWebApp/Controllers/ClubController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroopWebApp.Controllers
+{
+	public class ClubController1 : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ RunGroopWebApp/Controllers/RaceController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroopWebApp.Controllers
+{
+	public class RaceController1 : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
