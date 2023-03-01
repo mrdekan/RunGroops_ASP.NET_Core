fe8151bb0b48bf16d94e79f350fd60153e45f1d8
mrdekan <yeros05040504@gmail.com>
2023-03-01 15:48:45 +0200

    Depency injection + Repository pattern

--- RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -1,25 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
+using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
 
 namespace RunGroopWebApp.Controllers
 {
 	public class ClubController : Controller
 	{
-		private readonly ApplicationDbContext _context;
-		public ClubController(ApplicationDbContext context)
+		private readonly IClubRepository _clubRepository;
+		public ClubController(IClubRepository clubRepository)
 		{
-			this._context = context;
+			_clubRepository= clubRepository;
 		}
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
-			var clubs = _context.Clubs.ToList();
+			var clubs = await _clubRepository.GetAll();
 			return View(clubs);
 		}
-		public IActionResult Detail(int id)
+		public async Task<IActionResult> Detail(int id)
 		{
-			Club club = _context.Clubs.Include(a=>a.Address).FirstOrDefault(c=>c.Id == id);
+			Club club = await _clubRepository.GetByIdAsync(id);
 			return View(club);
 		}
 	}
