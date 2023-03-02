f774700717a2acab5948442f3ba7567f4a1296ef
mrdekan <yeros05040504@gmail.com>
2023-03-02 09:14:46 +0200

    Added photo uploading for races

--- RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -4,15 +4,19 @@ using RunGroopWebApp.Data;
 using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
 using RunGroopWebApp.Repository;
+using RunGroopWebApp.Services;
+using RunGroopWebApp.ViewModels;
 
 namespace RunGroopWebApp.Controllers
 {
 	public class RaceController : Controller
 	{
 		private readonly IRaceRepository _raceRepository;
-		public RaceController(IRaceRepository raceRepository)
+		private readonly IPhotoService _photoService;
+		public RaceController(IRaceRepository raceRepository, IPhotoService photoService)
 		{
 			_raceRepository = raceRepository;
+			_photoService = photoService;
 		}
 		public async Task<IActionResult> Index()
 		{
@@ -29,11 +33,31 @@ namespace RunGroopWebApp.Controllers
 			return View();
 		}
 		[HttpPost]
-		public async Task<IActionResult> Create(Race race)
+		public async Task<IActionResult> Create(CreateRaceViewModel raceVM)
 		{
-			if (!ModelState.IsValid) return View(race);
-			_raceRepository.Add(race);
-			return RedirectToAction("Index");
+			if (ModelState.IsValid)
+			{
+				var result = await _photoService.AddPhotoAsync(raceVM.Image);
+				var race = new Race
+				{
+					Title = raceVM.Title,
+					Description = raceVM.Description,
+					Image = result.Url.ToString(),
+					Address = new Address
+					{
+						Street = raceVM.Address.Street,
+						City = raceVM.Address.City,
+						State = raceVM.Address.State,
+					},
+				};
+				_raceRepository.Add(race);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				ModelState.AddModelError("", "Photo upload failed");
+			}
+			return View(raceVM);
 		}
 	}
 }
